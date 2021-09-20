1aa6e17cf1e530f145331a5c4ca43417b7893e89
CryNext141 <90177764+CryNext141@users.noreply.github.com>
2021-09-20 18:36:07 +0300

    141

---
+++ BooleanTesting.java
@@ -0,0 +1,26 @@
+class BooleanTesting{
+
+    public static void main(String[] args){
+
+        boolean bool = false;
+
+        System.out.println(bool);
+
+        bool = true;
+
+        System.out.println(bool);
+
+        if (bool) {
+            System.out.println("Hi))");
+        }
+        bool = false;
+
+        if (!bool) {
+            System.out.println("Damn");
+        }
+
+
+
+    }
+
+}
---
+++ CharDemo2.java
@@ -0,0 +1,20 @@
+class CharDemo2{
+
+    public static void main(String[] args){
+
+        char char1 = 'x';
+
+        System.out.println(char1);
+
+        char1++;
+
+        System.out.println(char1);
+
+
+
+
+
+
+    }
+
+}
